73a97dddb032acaa8f8b27a03c3be9a1883737b5
Carlos E R Santiago <carlosers@gmail.com>
2022-04-22 21:02:55 -0300

    add: verify if numbers are NaN, finity or infinity

--- src/Core/Funtamental/DataTypes/NumberFloatingPoint.java
+++ src/Core/Funtamental/DataTypes/NumberFloatingPoint.java
@@ -15,5 +15,22 @@ public class NumberFloatingPoint {
 
         System.out.printf("Número do tipo float: %f\n",numFloat);
         System.out.printf("Numero do tipo double: %f\n",numDob);
+
+        // para testes se um numero é not a number, finito ou infinito
+        if (Double.isNaN(Math.sqrt(-20))){
+            System.out.println("Erro, sou um NaN");
+        }
+        if (Double.isNaN(0.0/0.0)){
+            System.out.println("Erro, sou um NaN");
+        }
+
+        if (Double.isFinite(numDob))
+            System.out.println("Opa, sou um número finito!");
+
+        if (Double.isInfinite(1.0/0.0)){
+            System.out.println("Erro, sou um número infinito");
+        }
+        if (Double.isInfinite(-1.0/0.0))
+            System.out.println("Erro, sou um número negativo infinito!");
     }
 }
