c2128d7dfb0337e0d4f77d181670e86ffc6f6bd2
Carlos E R Santiago <carlosers@gmail.com>
2022-04-22 23:00:17 -0300

    add: how to use char type

---
+++ src/Core/Funtamental/DataTypes/charLiteral.java
@@ -0,0 +1,15 @@
+package Core.Funtamental.DataTypes;
+
+/**
+ * show how to use char type and scape code
+ * @version 1.04 2022-04-22
+ * @author Carlos Santiago
+ */
+public class charLiteral {
+    public static void main(String[] args) {
+        char letra = 'C';
+        char codigo = '\u0041';
+        System.out.printf("A primeira letra do meu nome é %c \ne %c é uma vogal",letra,codigo);
+
+    }
+}
